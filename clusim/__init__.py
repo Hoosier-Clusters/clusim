9f07ecc5b2459426cd480ae9f200e6e054467401
Alex Gates <ajgates42@gmail.com>
2018-07-13 10:42:18 -0400

    completely remoted import from init

--- clusim/__init__.py
+++ clusim/__init__.py
@@ -12,20 +12,3 @@ __author__ = """\n""".join([
 __version__ = '0.3'
 __release__ = '0.3'
 
-from clusim.clustering import *
-#import clusim.clustering
-
-from clusim.clugen import *
-#import clusim.clugen
-from clusim.dag import *
-#import clusim.dag
-
-from clusim.sim import *
-#import clusim.sim
-from clusim.clusimelement import *
-#import clusim.clusimelement
-
-from clusim.plotutils import *
-#import clusim.plotutils
-from clusim.utils import *
-#import clusim.utils
