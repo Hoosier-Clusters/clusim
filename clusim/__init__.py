e330720514a359455dcddd0fe71d2eff5d1de64c
Alex Gates <ajgates42@gmail.com>
2018-07-14 07:24:32 -0400

    fixed sync between __init__ and __conf__

--- clusim/__init__.py
+++ clusim/__init__.py
@@ -1,14 +1,14 @@
 __package__ = 'clusim'
 __title__ = 'CluSim: A python package for clustering similarity'
-__description__ = 'This package implements a series of methods to compare disjoint, overlapping, and hierarchical clusterings.'
+__description__ = 'This package implements a series of methods to compare \
+    disjoint, overlapping, and hierarchical clusterings.'
 
 __copyright__ = '2017, Gates, A.J., Ahn YY'
 
 __author__ = """\n""".join([
-	'Alexander J Gates <ajgates42@gmail.com>',
-	'YY Ahn <yyahn@iu.edu>'
+    'Alexander J Gates <ajgates42@gmail.com>',
+    'YY Ahn <yyahn@iu.edu>'
 ])
 
 __version__ = '0.3'
 __release__ = '0.3'
-
