3bac2ab5f525adda7ec2085c188370a5a99dc2fc
Alex Gates <ajgates42@gmail.com>
2018-08-23 00:11:20 -0400

    built html

---
+++ docs/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to CluSim\u2019s documentation!"],titleterms:{clusim:0,document:0,indic:0,tabl:0,welcom:0}})
